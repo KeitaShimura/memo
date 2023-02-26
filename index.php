21f7f04d4cedf85843b106684702c0b76c709ed3
KeitaShimura <keitashimura2023@gmail.com>
2023-02-26 17:00:46 +0900

    mysqlへの接続

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Practice</h2>
+    <?php
+    try {
+        $db = new PDO('mysql:dbname=mydb; host=127.0.0.1; charset=utf8','root', '');
+
+    } catch (PDOException $e){
+        echo 'DB接続エラー:' . $e->getMessage();
+    }
+
+    $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="桃", price=210, keyword="缶詰,ピンク,甘い", sales=0, created="2018-01-23", modified="2018-01-23"');
+    echo $count .'件のデータを挿入しました';
+    ?>
+</body>
+</html>
